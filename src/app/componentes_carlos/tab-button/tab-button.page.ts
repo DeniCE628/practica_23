dc0eeb74ec38a195c6de5ed5b812fc2b052a7ec3
DeniCE628 <hptablet098@gmail.com>
2023-03-06 23:07:10 -0600

    componentes carlos

---
+++ src/app/componentes_carlos/tab-button/tab-button.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-button',
+  templateUrl: './tab-button.page.html',
+  styleUrls: ['./tab-button.page.scss'],
+})
+export class TabButtonPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
